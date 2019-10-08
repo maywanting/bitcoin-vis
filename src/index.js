a96654be554e9440aca5275864ed31f183de9d28
may <maywanting@gmail.com>
2019-10-09 03:46:16 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,91 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '../node_modules/react-vis/dist/style.css';
+import {
+    XYPlot,
+    LineSeries,
+    VerticalGridLines,
+    HorizontalGridLines,
+    XAxis,
+    YAxis,
+} from 'react-vis';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            estimated: [],
+            min: [],
+            income: [],
+            priceInUSD: [],
+            value: [],
+        };
+    }
+    componentDidMount() {
+        return fetch('../value.json').then(response => response.json()).then(jsondata => {
+            let estimated = [];
+            let min = [];
+            let income = [];
+            let priceInUSD = [];
+            let value = [];
+            let num = 0;
+            for (let date in jsondata) {
+                estimated.push({x: num, y: parseFloat(jsondata[date]['estimated'])});
+                min.push({x: num, y: parseFloat(jsondata[date]['min'])});
+                income.push({x: num, y: parseFloat(jsondata[date]['income'])});
+                priceInUSD.push({x: num, y: parseFloat(jsondata[date]['priceInUSD'])});
+                value.push({x: num, y: parseFloat(jsondata[date]['value'])});
+                num++;
+            }
+            this.setState({
+                estimated: estimated,
+                min: min,
+                income: income,
+                priceInUSD: priceInUSD,
+                value: value,
+            });
+            console.log(this.state.estimated);
+        });
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <div className="App">
+                <XYPlot height={200} width={1200}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis title="每年电力消耗值"/>
+                    <YAxis title="TWh"/>
+                    <LineSeries data={this.state.estimated} />
+                    <LineSeries data={this.state.min} />
+                </XYPlot>
+                <XYPlot height={200} width={1200}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis title="每天的块收益加和"/>
+                    <YAxis title="BTC"/>
+                    <LineSeries data={this.state.income} />
+                </XYPlot>
+                <XYPlot height={200} width={1200}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis title="bitcoin每日的平均价格"/>
+                    <YAxis title="BTC/USD"/>
+                    <LineSeries data={this.state.priceInUSD} />
+                </XYPlot>
+                <XYPlot height={200} width={1200}>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <XAxis title="计算的每日算力总价值"/>
+                    <YAxis/>
+                    <LineSeries data={this.state.value} />
+                </XYPlot>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
